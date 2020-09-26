171880cdf37c99887ede378ee4794576dd1f75eb
Taha Bohra <tahamustafa053@gmail.com>
2020-09-26 23:44:07 +0530

    bug fix

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,34 +1,83 @@
-const submit_btn = document.getElementById("submit_btn");
+const add_btn = document.getElementById("add-btn");
 var input = document.getElementById("task_input");
 var task_list = document .getElementById("task-list");
 var date_time = document.getElementById("date-time");
-var correct_btn = document.getElementById("correct-btn");
+var correct_btn = document.querySelectorAll("#correct-btn");
+var counter_text = document.querySelector(".small");
+
+
+console.log(counter_text.innerHTML);
 
 input.value = "";
 input.name = "";
 
 date_time.innerHTML = moment().format('MMMM Do YYYY dddd');
-var count = 0;
-$("#submit_btn").on("click", function() {
-    var task = input.value;
-    console.log(task);
 
-    if (count == 0) {
-        $("#task-list").append("<div class='list-item-container current'><li class='list-item'>" + task + "<span><button class='btn' id='wrong-btn'><i class='fas fa-times-circle'></i></button><button class='btn' id='correct-btn'><i class='fas fa-check-circle'></i></button></span></li></div>")
-        count++;
-    } else {
-        $("#task-list").append("<div class='list-item-container'><li class='list-item'>" + task + "<span><button class='btn' id='wrong-btn'><i class='fas fa-times-circle'></i></button><button class='btn' id='correct-btn'><i class='fas fa-check-circle'></i></button></span></li></div>")
-        count++;
-    }
-    // var node = document.createElement("div");
-    // var node1 = document.createElement("li");
-    // var textnode = document.createTextNode(task);
-    // node1.appendChild(textnode);
-    // node.appendChild(node1);
-    // document.getElementById("task-list").appendChild(node);
-    input.value = "";
-    input.name = "";
+var count = 0;
+var id = 4000;
+tasks = [];
+
+function show_task(tasks) {
+	tasks.forEach(function(task) {
+		$("#task-list").append("<div class='task-list-element'><li>" + task.task + "<span><button class='btn' id='wrong-btn'><i class='fas fa-times-circle'></i></button><button class='btn' id='correct-btn'><i class='fas fa-check-circle'></i></button></span></li></div>");
+	});
+}
+
+// $("input[type='text'").keypress(function(event) {
+// 	if (event.which === 13) {
+
+// 	}
+// })
+
+$("#add-btn").on("click", function() {
+	var task = input.value;
+	$("#task-list").append("<div class='task-list-element' id='" + id + "'><li>" + task + "<span><button class='btn' id='wrong-btn'><i class='fas fa-times-circle'></i></button><button class='btn' id='correct-btn'><i class='fas fa-check-circle'></i></button></span></li></div>");
+	tasks.push({"task": task, "id": id});
+	id++;
+	console.log(tasks);
+	count++;
+	counter_text.innerHTML = "You have " + count + " tasks left";
+	input.value = "";
+	input.name = "";
 });
+
+
+$(document).on("click", "#correct-btn", function() {
+	var list_text = document.querySelector(".task-list-element li");
+	console.log(list_text.textContent);
+	// $(".task-list-element").addClass("completed");
+	console.log(list_text);
+
+	tasks.forEach(function(task) {
+		if (list_text.textContent == task) {
+			console.log("this will be deleted");
+		}
+	});
+	// $(this).addClass("temp");
+	// $("#correct-btn").on("click", function() {
+	// 	$(this).addClass("completed");
+	// });
+});
+
+$("#correct-btn").click(function(){
+	$(this).parent().remove();
+});
+
+  
+
+// correct_btn.forEach(function(btn) {
+//     btn.addEventListener("click", function() {
+//         alert("Hello world");
+//     });
+// });
+
+
+// $("span").find("#correct-btn").forEach(function(btn) {
+// 	btn.addEventListener("click", function() {
+// 		alert("hello world");
+// 	});
+// });
+
 
 
 /* Animations on scroll */
@@ -38,11 +87,17 @@ $("#submit_btn").on("click", function() {
 //     offset: '50%'
 // });
 
+// add_btn.addEventListener("click", function() {
+//     var task = input.value;
+//     console.log(task);
 
-
-
-
-
+//     var node = document.createElement("div");
+//     var node1 = document.createElement("li");
+//     var textnode = document.createTextNode(task);
+//     node1.appendChild(textnode);
+//     node.appendChild(node1);
+//     document.getElementById("task-list").appendChild(node);
+// });
 
 // functionality added on 2nd AUG
 // added jquery
